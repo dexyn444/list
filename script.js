b67383934b8395e766c5d8c0c8d3a840220e750c
dexyn444 <105306981+dexyn444@users.noreply.github.com>
2025-07-08 17:37:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ function enviarWhatsApp() {
 
   const mensagem = "🛒 Minha lista de compras:\n\n" +
     listaDeCompras.map((item, i) => {
-      return `${item.riscado ? '✅' : '🛒'} ${i + 1}. ${item.nome}`;
+      return `${item.riscado ? '✅' : '✅'} ${i + 1}. ${item.nome}`;
     }).join("\n");
 
   const url = `https://wa.me/?text=${encodeURIComponent(mensagem)}`;
